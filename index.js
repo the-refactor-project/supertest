b1bb2d2a1749d7f1a689b4cb537dc0776a43fae3
Mario González <mario@mariogl.com>
2025-05-14 09:21:20 +0200

    feat(methods): add alias 'patata' for post method

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 /**
  * Module dependencies.
  */
-const methods = require('methods');
+const methods = require("methods");
 let http2;
 try {
-  http2 = require('http2'); // eslint-disable-line global-require
+  http2 = require("http2"); // eslint-disable-line global-require
 } catch (_) {
   // eslint-disable-line no-empty
 }
-const Test = require('./lib/test.js');
-const agent = require('./lib/agent.js');
+const Test = require("./lib/test.js");
+const agent = require("./lib/agent.js");
 
 /**
  * Test against the given `app`,
@@ -21,21 +21,21 @@ const agent = require('./lib/agent.js');
  * @return {Test}
  * @api public
  */
-module.exports = function(app, options = {}) {
+module.exports = function (app, options = {}) {
   const obj = {};
 
-  if (typeof app === 'function') {
+  if (typeof app === "function") {
     if (options.http2) {
       if (!http2) {
         throw new Error(
-          'supertest: this version of Node.js does not support http2'
+          "supertest: this version of Node.js does not support http2"
         );
       }
     }
   }
 
-  methods.forEach(function(method) {
-    obj[method] = function(url) {
+  methods.forEach(function (method) {
+    obj[method] = function (url) {
       var test = new Test(app, method, url, options.http2);
       if (options.http2) {
         test.http2();
@@ -46,6 +46,7 @@ module.exports = function(app, options = {}) {
 
   // Support previous use of del
   obj.del = obj.delete;
+  obj.patata = obj.post;
 
   return obj;
 };
